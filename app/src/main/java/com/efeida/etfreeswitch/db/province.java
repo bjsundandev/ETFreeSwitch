3b5e0c74e39011b451dc0c1046fd952952c0b28c
sundan <bjsundan@foxmail.com>
2017-06-23 10:36:07 +0800

    加入数据库和表的各项配置

---
+++ app/src/main/java/com/efeida/etfreeswitch/db/province.java
@@ -0,0 +1,31 @@
+package com.efeida.etfreeswitch.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by bjsundan on 2017/6/23.
+ */
+
+public class province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode=provinceCode;
+    }
+}
